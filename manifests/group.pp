26e39a999f746f2cbbc84b0963c40ad6b1132e24
John Bond <github@johnbond.org>
2018-05-23 17:35:52 +0100

    update rancid module

---
+++ manifests/group.pp
@@ -0,0 +1,21 @@
+# == Define: rancid::group
+#
+define rancid::group (
+  Array[Rancid::Device] $devices,
+) {
+  $delimiter = $rancid::router_db_delimiter
+  exec { "rancid-cvs-${name}":
+    command => "rancid-cvs ${name}",
+    path    => $rancid::rancid_path_env,
+    user    => $rancid::user,
+    creates => "${rancid::homedir}/${name}/CVS",
+  }
+  file { "${rancid::homedir}/${name}/router.db":
+    ensure  => 'file',
+    owner   => $rancid::user,
+    group   => $rancid::group,
+    mode    => '0640',
+    content => template('rancid/router.db.erb'),
+    require => Exec["rancid-cvs-${name}"],
+  }
+}
